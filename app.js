4c0748b69fa88e85d1a42990261000b18f90cdab
cgkaczenski <kzpulaski@gmail.com>
2016-04-06 18:36:18 -0500

    init

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	exphbs = require('express-handlebars'),
+	expressValidator = require('express-validator'),
+	session = require('express-session'),
+	flash = require('connect-flash'),
+	multer = require('multer'),
+	upload = multer({ dest: './public/images/portfolio' });
+
+var routes = require('./routes/index'),
+	admin = require('./routes/admin');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle Sessions
+app.use(session({
+  secret:'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Connect Flash
+app.use(flash());
+
+app.use('/', routes);
+app.use('/admin', admin);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port: '+app.get('port'));
+});
